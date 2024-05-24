df075b75c203729a0717def0a43bcc0ae7467f54
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-23 22:46:23 -0300

    commit 5

--- arquitetura/src/Aluno.php
+++ arquitetura/src/Aluno.php
@@ -7,4 +7,10 @@ class Aluno
   private CPF $cpf;
   private string $nome;
   private Email $email;
+  private array $telefones;
+
+  public function adicionarTelefone(string $ddd, string $numero)
+  {
+    $this->telefones[] = new Telefone($ddd, $numero);
+  }
 }
