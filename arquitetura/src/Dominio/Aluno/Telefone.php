48fc58435c9baca59b3d17500ace2bfb2c82328d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-25 14:30:13 -0300

    commit 9

--- arquitetura/src/Dominio/Aluno/Telefone.php
+++ arquitetura/src/Dominio/Aluno/Telefone.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Alura\Arquitetura\Dominio\Aluno;
+
 use InvalidArgumentException;
 
 class Telefone
@@ -8,7 +9,8 @@ class Telefone
   private string $ddd;
   private string $numero;
 
-  public function __construct(string $ddd, string $numero) {
+  public function __construct(string $ddd, string $numero)
+  {
     $this->setDDD($ddd);
     $this->setNumero($numero);
   }
@@ -18,7 +20,7 @@ class Telefone
     if (preg_match('/\d{2}/', $ddd) !== 1) {
       throw new InvalidArgumentException('DDD inválido');
     }
-    
+
     $this->ddd = $ddd;
   }
 
@@ -29,6 +31,16 @@ class Telefone
     }
 
     $this->numero = $numero;
+  }
+
+  public function ddd(): string
+  {
+    return $this->ddd;
+  }
+
+  public function numero(): string
+  {
+    return $this->numero;
   }
 
   public function __toString(): string
